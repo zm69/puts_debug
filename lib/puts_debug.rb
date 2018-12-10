9f20b3bc68f238dd01474e344e0ce82c935dd652
oleh <oleh.novosad@gmail.com>
2018-12-10 15:59:16 -0500

    Updated syntax, added color

--- lib/puts_debug.rb
+++ lib/puts_debug.rb
@@ -1,7 +1,6 @@
 require "puts_debug/version"
 
 module Kernel
-
   ##
   # For easier puts debugging (pd). Defined in Kernel module which is included into Object class so this method
   # is accessible from everywhere. Output is sent to standard error stream (stderr) so it could be further separated
@@ -18,44 +17,92 @@ module Kernel
   #   Examples:
   #
   #     pd('something') =>
+  #     ============================================================
+  #     something
+  #     ============================================================
+  #
+  #     pd(data: 'something', data_color: 'magenta', lines: :none) =>
   #     something
   #
   #     pd(name: 'Yogurt') =>
+  #     ============================================================
   #     name: Yogurt
+  #     ============================================================
   #
-  #     pd(1, '-', :both, 1) =>
+  #     pd(data: 1, symbol: '-', margin: 1) =>
   #
   #     ------------------------------------------------------------
   #     1
   #     ------------------------------------------------------------
   #
-  #     pd({a: 1, b: 3}, '*', :both, 2) =>
-  #
-  #
+  #     pd(data: {a: 1, b: 3}, symbol: '*') =>
   #     ************************************************************
   #     a: 1
   #     b: 3
   #     ************************************************************
-  #
-  #
-  def pd(h = {}, line_symbol = nil, show_lines = :both, empty_lines_margin = nil)
+  def pd(args)
+    params = nil
 
-    return unless Rails.env.development?
-
-    $stderr.puts "\n" * empty_lines_margin if empty_lines_margin.present? && show_lines.in?([:both, :top])
-    $stderr.puts (line_symbol * 60) if line_symbol.present? && show_lines.in?([:both, :top])
-
-    if h.is_a? Hash
-      h.each do |k, v|
-        $stderr.puts "#{k}: #{v}"
+    if args.is_a? Hash
+      if args[:data].nil?
+        params = { data: args }
+      else
+        params = args
       end
     else
-      $stderr.puts h
+      params = { data: args }
     end
 
-    $stderr.puts line_symbol * 60 if line_symbol.present? && show_lines.in?([:both, :bottom])
-    $stderr.puts "\n" * empty_lines_margin if empty_lines_margin.present? && show_lines.in?([:both, :bottom])
+    # parameters
+    data       = params[:data]
+    symbol     = params[:symbol] || '='
+    line_color = params[:color] || 'red'
+    data_color = params[:data_color] || nil
+    margin     = params[:margin] || 0
+    lines      = params[:lines] || :both
+    title      = params[:title]
+    epilogue   = params[:epilogue]
 
+    # locals
+    line_str   = Rainbow(symbol * 60).try(line_color)
+    margin_str = "\n" * margin
+
+    $stderr.puts margin_str if margin > 0 && lines.in?([:both, :top])
+    $stderr.puts line_str if lines.in?([:both, :top])
+
+    if title.present?
+      title_str = if title.ends_with? ':'
+        "#{title}\n"
+      else
+        "#{title}:\n"
+      end
+
+      $stderr.puts Rainbow(title_str).try(line_color)
+    end
+
+    data_str = case data
+    when Hash
+      data.map { |k, v| "#{k}: #{v}" }.join("\n")
+    when Array
+      data.map { |v| "#{v}" }.join(",\n")
+    else
+      data.to_s
+    end
+
+    data_str = Rainbow(data_str).try(data_color) if data_color.present?
+
+    $stderr.puts data_str
+
+    if title.present?
+      $stderr.puts ''
+    end
+
+    $stderr.puts Rainbow(epilogue).try(line_color) if epilogue.present?
+
+    if lines.present?
+      $stderr.puts line_str if lines.in?([:both, :bottom])
+      $stderr.puts margin_str if margin > 0 && lines.in?([:both, :bottom])
+    end
   end
 
 end
